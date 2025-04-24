1ae0158a940bde47533ba91561270fe91d7cc74c
Lucas-Noguera <lucas2005n@gmail.com>
2025-04-24 11:56:05 -0300

    Codigo refactorizado

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,4 +1,23 @@
+import { useState } from 'react'
+import { navLinks } from '../contants'
+
+const NavItems = () => {
+    return(
+        <ul>
+            {navLinks.map((item) => (
+                <li key={item.id} className='nav-li'></li>
+            ))}
+        </ul>
+    )
+}
+
 export const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsOpen((prevIsOpen) => !prevIsOpen)
+    }
+
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
         <div className="max-w-7xl mx-auto">
@@ -6,9 +25,14 @@ export const Navbar = () => {
                 <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
                     Lucas
                 </a>
-                <button>
-                    <img src="" alt="" />
+                <button onClick={toggleMenu} className='text-neutral-400 hover:text-white focus:outline-none sm:hidden flex' aria-label='Toggle menu'>
+                    <img src={ isOpen ? '/assets/close.svg': 'assets/menu.svg'} alt="menu" className="w-6 h-6" />
                 </button>
+
+                <nav className='sm:hidden flex'>
+                    <NavItems />
+                </nav>
+
             </div>
         </div>
     </header>
