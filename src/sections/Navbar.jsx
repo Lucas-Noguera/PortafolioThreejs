171e593ed648b64f0ed1c7a30cbfc2bf85a103dd
Lucas-Noguera <lucas2005n@gmail.com>
2025-04-24 14:59:11 -0300

    Navgar con renderizado condicional responsivo agregado

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
-import { navLinks } from '../contants'
+import { navLinks } from '../constants'
 
 const NavItems = () => {
     return(
-        <ul>
+        <ul className='nav-ul'>
             {navLinks.map((item) => (
-                <li key={item.id} className='nav-li'></li>
+                <li key={item.id} className='nav-li'>
+                    <a href={item.href} className='nav-li_a' onClick={() => {}}>
+                        {item.name}
+                    </a>
+                </li>
             ))}
         </ul>
     )
@@ -20,20 +24,39 @@ export const Navbar = () => {
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+
         <div className="max-w-7xl mx-auto">
+
             <div className="flex justify-between items-center py-5 mx-auto c-space">
-                <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
+
+                <a href="/"
+                 className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
                     Lucas
                 </a>
-                <button onClick={toggleMenu} className='text-neutral-400 hover:text-white focus:outline-none sm:hidden flex' aria-label='Toggle menu'>
-                    <img src={ isOpen ? '/assets/close.svg': 'assets/menu.svg'} alt="menu" className="w-6 h-6" />
+
+                <button
+                onClick={toggleMenu}
+                className='text-neutral-400 hover:text-white focus:outline-none sm:hidden flex'
+                aria-label='Toggle menu'>
+
+                    <img
+                    src={ isOpen ? '/assets/close.svg': 'assets/menu.svg'}
+                    alt="menu"
+                    className="w-6 h-6" />
+
                 </button>
 
-                <nav className='sm:hidden flex'>
+                <nav className=' sm:flex hidden'>
                     <NavItems />
                 </nav>
 
             </div>
+
+            <nav className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+                    <nav className='p-5'>
+                    <NavItems />
+                    </nav>
+                </nav>
         </div>
     </header>
   )
