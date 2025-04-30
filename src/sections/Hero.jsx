72be4988273f7cecb50f31132670665ce024c59f
Lucas-Noguera <lucas2005n@gmail.com>
2025-04-30 14:54:33 -0300

    Hero section terminado

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,8 +3,22 @@ import { Canvas } from '@react-three/fiber'
 import { HackerRoom } from '../../components/HackerRoom'
 import { Suspense } from 'react'
 import { CanvasLoader } from '../../components/CanvasLoader'
+import { Leva } from 'leva'
+import { useMediaQuery } from 'react-responsive'
+import { calculateSizes } from '../constants'
+import { Target } from '../../components/Target'
+import { ReactLogo } from '../../components/ReactLogo'
+import { Cube } from '../../components/Cube'
+import { Rings } from '../../components/Rings'
+import { HeroCamera } from '../../components/HeroCamera'
 
 export const Hero = () => {
+    const isSmall = useMediaQuery({ maxWidth: 440 })
+    const isMobile = useMediaQuery({ maxWidth: 768 })
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 })
+
+    const sizes = calculateSizes(isSmall, isMobile, isTablet)
+
     return (
         <section className="min-h-screen w-full flex flex-col relative">
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -21,12 +35,41 @@ export const Hero = () => {
             </div>
 
              <div className="w-full h-full absolute inset-0">
+               {/*<Leva />*/}
                 <Canvas className='w-full h-full'>
-                    <Suspense  fallback={<CanvasLoader/>}>
-                    <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-                    <HackerRoom scale={0.07} position={[0, -5, 0]} rotation={[0, 280, 0]} />
-                    <ambientLight intensity={2} />
+                    <Suspense
+                    fallback={<CanvasLoader/>}>
+
+                    <PerspectiveCamera
+                    makeDefault
+                    position={[0, 0, 20]} />
+
+                    <HeroCamera isMobile={isMobile}>
+
+                    <HackerRoom
+                    position={sizes.deskPosition}
+                    rotation={[0, -Math.PI, 0]}
+                    scale={sizes.deskScale}
+                    />
+
+                    </HeroCamera>
+
+                    <group>
+
+                        <Target position={sizes.targetPosition} />
+
+                        <ReactLogo position={sizes.reactLogoPosition} />
+
+                        <Cube position={sizes.cubePosition} />
+
+                        <Rings position={sizes.ringPosition} />
+
+                    </group>
+
+                    <ambientLight
+                    intensity={2} />
                     <directionalLight position={[10, 10, 10]} intensity={0.5} />
+
                     </Suspense>
                 </Canvas>
              </div>
