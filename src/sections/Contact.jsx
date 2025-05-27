0cb84dc5844ca56d379d81b71a7975f84bee55e1
Lucas-Noguera <lucas2005n@gmail.com>
2025-05-27 11:31:30 -0300

    New Deploy

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -64,12 +64,16 @@ const handleSubmit = (e) => {
     <section className="c-space my-20" id="contact">
       {alert.show && <Alert {...alert} />}
 
-        <div className="relative min-h-screen flex items-center justify-center flex-col">
-                 <img src="/assets/terminal.png" alt="terminal-bg" className="absolute inset-0 min-h-screen" />
+        <div className="relative w-full min-h-screen px-4 py-12 flex justify-center"
+        style={{ backgroundImage: "url('/assets/terminal.png')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+        }}>
 
             <div className="contact-container">
-            <h3 className="head-text">Let's talk</h3>
-            <p className="text-lg text-white-600 mt-3">
+            <h3 className="head-text text-white">Let's talk</h3>
+            <p className="text-lg text-white mt-3">
               Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
               life, I’m here to help.
             </p>
