da715910e210c2c4c67d91f5aa51f52141bf4dd7
Lucas-Noguera <lucas2005n@gmail.com>
2025-06-10 10:07:00 -0300

    New Deploy

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -30,7 +30,7 @@ export const Projects = () => {
             <p className="head-text">My Work</p>
             <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full">
                 <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-block-200">
-                    <div className="absolute top-0 right-0 ">
+                    <div className="absolute top-0 right-0 z-0">
                         <img src={currentProject.spotlight} alt="spotlight" className="w-full h-96 object-cover rounded-xl" />
                     </div>
                     <div className='p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg' style={myProjects[0].logoStyle}>
