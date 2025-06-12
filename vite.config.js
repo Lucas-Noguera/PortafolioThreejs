f20586ef924e9292d414f12ee45bfacbd22d1a0c
Lucas-Noguera <lucas2005n@gmail.com>
2025-06-12 13:41:29 -0400

    Commit Actualizacion VIte config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,25 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    splitVendorChunkPlugin(), // esto separa automáticamente tus dependencias mayores
-  ],
+  plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        // opcionalmente puedes agrupar manualmente:
-        manualChunks: {
-          drei: ['@react-three/drei', '@react-three/fiber', 'three'],
+        manualChunks(id) {
+          // Todo lo que venga de node_modules lo aisla
+          if (id.includes('node_modules')) {
+            // Agrupa three, fiber y drei juntos en un chunk “three-vendor”
+            if (
+              id.includes('three/') ||
+              id.includes('@react-three/fiber') ||
+              id.includes('@react-three/drei')
+            ) {
+              return 'three-vendor'
+            }
+            // El resto de dependencias en “vendor”
+            return 'vendor'
+          }
         },
       },
     },
