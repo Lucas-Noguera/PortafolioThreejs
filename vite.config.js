d5a72cce68357155592945377a0a9b9ec7a95868
Lucas-Noguera <lucas2005n@gmail.com>
2025-06-12 13:53:33 -0400

    Rollback

--- vite.config.js
+++ vite.config.js
@@ -2,31 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ],
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // 1) React y ReactDOM en su propio chunk
-            if (
-              id.match(/node_modules\/react($|\/)/) ||
-              id.match(/node_modules\/react-dom($|\/)/) ||
-              id.includes('node_modules/scheduler')
-            ) {
-              return 'react-vendor'
-            }
-            // 2) three + fiber + drei
-            if (
-              id.includes('three/') ||
-              id.includes('@react-three/fiber') ||
-              id.includes('@react-three/drei')
-            ) {
-              return 'three-vendor'
-            }
-            // 3) resto de libs
-            return 'vendor'
-          }
+        // opcionalmente puedes agrupar manualmente:
+        manualChunks: {
+          drei: ['@react-three/drei', '@react-three/fiber', 'three'],
         },
       },
     },
