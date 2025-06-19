2f824ee0cfa83c40b4dec378ddeb07e97b5dc535
Lucas-Noguera <lucas2005n@gmail.com>
2025-06-19 11:33:13 -0300

    Flechas de desplazamiento fijadas

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -29,7 +29,7 @@ export const Projects = () => {
         <section className="c-space my-20">
             <p className="head-text">My Work</p>
             <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full">
-                <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-block-200">
+                <div className="flex flex-col justify-between h-full relative sm:p-10 py-10 px-5 shadow-2xl shadow-block-200">
                     <div className="absolute top-0 right-0 z-0">
                         <img src={currentProject.spotlight} alt="spotlight" className="w-full h-96 object-cover rounded-xl" />
                     </div>
